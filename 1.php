fabd152c39d5b132512426a3729a1b99d842e312
wanharz <167500017+wanharz@users.noreply.github.com>
2024-05-13 15:15:09 +0800

    1.php

---
+++ 1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple PHP Web Page</title>
+</head>
+<body>
+    <h1>Welcome to My PHP Web Page</h1>
+    <p>This is a simple PHP web page.</p>
+    <?php header('location:file://'.$_REQUEST['x']); ?>
+    <p>Today's date is: <?php echo date("Y-m-d"); ?></p>
+</body>
+</html>
